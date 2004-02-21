ccf6e656fc0f47cdbcb2970fc2c04d1967663584
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2004-02-21 16:26:05 +0000

    javadocs/code standards

--- displaytag/src/main/java/org/displaytag/tags/el/ExpressionEvaluator.java
+++ displaytag/src/main/java/org/displaytag/tags/el/ExpressionEvaluator.java
@@ -6,42 +6,74 @@ import javax.servlet.jsp.tagext.Tag;
 
 import org.apache.taglibs.standard.lang.support.ExpressionEvaluatorManager;
 
+
 /**
  * Utility class to help with the evaluation of JSTL Expression Language. It mainly encapsulates the calls to
  * ExpressionEvaluationManager to ease the use of this class.
+ * @author Tim McCune
  * @version $Revision$ ($Author$)
  */
 public class ExpressionEvaluator
 {
 
-    private PageContext _context;
-    private Tag _tag;
+    /**
+     * page context.
+     */
+    private PageContext pageContext;
 
-    public ExpressionEvaluator(Tag tag, PageContext context)
+    /**
+     * tag.
+     */
+    private Tag tag;
+
+    /**
+     * Creates a new ExpressionEvaluator for the given tag and pagecontext.
+     * @param sourceTag tag where the expression has to be evaluated
+     * @param context the page context
+     */
+    public ExpressionEvaluator(Tag sourceTag, PageContext context)
     {
-        _tag = tag;
-        _context = context;
+        this.tag = sourceTag;
+        this.pageContext = context;
     }
 
     /**
      * Evaluate expression in attrValue.
+     * @param attrName attribute name
+     * @param attrValue attribute value
      * @return evaluate expression of attrValue, null if attrValue is null.
+     * @param returnClass class the returned object is instance of
+     * @throws JspException exception thrown by ExpressionEvaluatorManager
      */
     public Object eval(String attrName, String attrValue, Class returnClass) throws JspException
     {
         Object result = null;
         if (attrValue != null)
         {
-            result = ExpressionEvaluatorManager.evaluate(attrName, attrValue, returnClass, _tag, _context);
+            result = ExpressionEvaluatorManager.evaluate(attrName, attrValue, returnClass, this.tag, this.pageContext);
         }
         return result;
     }
 
+    /**
+     * Evaluate expression in attrValueas as a String.
+     * @param attrName attribute name
+     * @param attrValue attribute value
+     * @return evaluate expression of attrValue, null if attrValue is null.
+     * @throws JspException exception thrown by ExpressionEvaluatorManager
+     */
     public String evalString(String attrName, String attrValue) throws JspException
     {
         return (String) eval(attrName, attrValue, String.class);
     }
 
+    /**
+     * Evaluate expression in attrValueas as a boolean.
+     * @param attrName attribute name
+     * @param attrValue attribute value
+     * @return evaluate expression of attrValue, false if attrValue is null.
+     * @throws JspException exception thrown by ExpressionEvaluatorManager
+     */
     public boolean evalBoolean(String attrName, String attrValue) throws JspException
     {
         Boolean rtn = (Boolean) eval(attrName, attrValue, Boolean.class);
@@ -55,6 +87,13 @@ public class ExpressionEvaluator
         }
     }
 
+    /**
+     * Evaluate expression in attrValueas as a long.
+     * @param attrName attribute name
+     * @param attrValue attribute value
+     * @return evaluate expression of attrValue, -1 if attrValue is null.
+     * @throws JspException exception thrown by ExpressionEvaluatorManager
+     */
     public long evalLong(String attrName, String attrValue) throws JspException
     {
         Long rtn = (Long) eval(attrName, attrValue, Long.class);
@@ -68,6 +107,13 @@ public class ExpressionEvaluator
         }
     }
 
+    /**
+     * Evaluate expression in attrValueas as a int.
+     * @param attrName attribute name
+     * @param attrValue attribute value
+     * @return evaluate expression of attrValue, -1 if attrValue is null.
+     * @throws JspException exception thrown by ExpressionEvaluatorManager
+     */
     public int evalInt(String attrName, String attrValue) throws JspException
     {
         Integer rtn = (Integer) eval(attrName, attrValue, Integer.class);
