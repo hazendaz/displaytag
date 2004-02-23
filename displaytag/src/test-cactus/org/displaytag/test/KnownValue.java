f919cb2028390d7df8fa0cbb10e8ea3ba2fafd09
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2004-02-23 21:11:54 +0000

    changed constant value to make tests more readable

--- displaytag/src/test-cactus/org/displaytag/test/KnownValue.java
+++ displaytag/src/test-cactus/org/displaytag/test/KnownValue.java
@@ -7,20 +7,21 @@ package org.displaytag.test;
  */
 public class KnownValue
 {
-    /**
-     * constant for the "ant" property.
-     */
-    public static final String ANT = "acolumn";
 
     /**
-     * constant for the "bee" property.
+     * constant for the "ant" property (this is both the property name and value).
      */
-    public static final String BEE = "bcolumn";
+    public static final String ANT = "ant";
 
     /**
-     * constant for the "camel" property.
+     * constant for the "bee" property (this is both the property name and value).
      */
-    public static final String CAMEL = "ccolumn";
+    public static final String BEE = "bee";
+
+    /**
+     * constant for the "camel" property (this is both the property name and value).
+     */
+    public static final String CAMEL = "camel";
 
     /**
      * getter for the "ant" property.
