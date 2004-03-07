1c8fb10b7710728571b7ef8c516da74214c3aa1e
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2004-03-07 21:30:31 +0000

    added beanInfo for caption tag

---
+++ displaytag/src/main/java/org/displaytag/tags/CaptionTagBeanInfo.java
@@ -0,0 +1,15 @@
+package org.displaytag.tags;
+
+import org.displaytag.util.BeanInfoUtil;
+
+
+/**
+ * Needed to make the "class" tag attribute working.
+ * @author fgiust
+ * @version $Revision$ ($Author$)
+ * @see org.displaytag.util.BeanInfoUtil
+ */
+public class CaptionTagBeanInfo extends BeanInfoUtil
+{
+
+}
