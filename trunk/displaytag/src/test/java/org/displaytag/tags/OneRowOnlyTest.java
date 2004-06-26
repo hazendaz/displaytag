bdbffb25527c5e73cd7eaaa8ca0b4f1d611de0ea
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2004-06-26 14:03:43 +0000

    test with a single row

---
+++ trunk/displaytag/src/test/java/org/displaytag/tags/OneRowOnlyTest.java
@@ -0,0 +1,63 @@
+package org.displaytag.tags;
+
+import org.displaytag.test.DisplaytagCase;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import com.meterware.httpunit.WebTable;
+
+
+/**
+ * A table with a single row.
+ * @author Fabrizio Giustina
+ * @version $Revision$ ($Author$)
+ */
+public class OneRowOnlyTest extends DisplaytagCase
+{
+
+    /**
+     * Instantiates a new test case.
+     * @param name test name
+     */
+    public OneRowOnlyTest(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * @see org.displaytag.test.DisplaytagCase#getJspName()
+     */
+    public String getJspName()
+    {
+        return "onerow.jsp";
+    }
+
+    /**
+     * Checks for the expected values in columns.
+     * @param jspName jsp name, with full path
+     * @throws Exception any axception thrown during test.
+     */
+    public void doTest(String jspName) throws Exception
+    {
+
+        WebRequest request = new GetMethodWebRequest(jspName);
+
+        WebResponse response = runner.getResponse(request);
+
+        if (log.isDebugEnabled())
+        {
+            log.debug("RESPONSE: " + response.getText());
+        }
+
+        WebTable[] tables = response.getTables();
+
+        assertEquals("Expected one table in result.", 1, tables.length);
+
+        assertEquals("Bad number of generated columns.", 2, tables[0].getColumnCount());
+        assertEquals("Bad number of generated rows.", 2, tables[0].getRowCount());
+
+        assertEquals("Bad content in column 1.", "ant", tables[0].getCellAsText(1, 0));
+        assertEquals("Bad content in column 2.", "bee", tables[0].getCellAsText(1, 1));
+    }
+}
