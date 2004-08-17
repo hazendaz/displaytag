db6f6419bdf3b45122d00d9b22011649ae395168
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2004-08-17 17:32:49 +0000

    fix 1006229 - Proper encoding of URLs under JDK 1.4

--- displaytag/src/main/java/org/displaytag/util/DefaultRequestHelper.java
+++ displaytag/src/main/java/org/displaytag/util/DefaultRequestHelper.java
@@ -1,5 +1,6 @@
 package org.displaytag.util;
 
+import java.lang.reflect.Method;
 import java.net.URLEncoder;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -22,6 +23,26 @@ import org.apache.commons.logging.LogFactory;
  */
 public class DefaultRequestHelper implements RequestHelper
 {
+
+    /**
+     * j2se 1.4 encode method, used by reflection if available.
+     */
+    private static Method encodeMethod14;
+
+    static
+    {
+        // URLEncoder.encode(String) has been deprecated in J2SE 1.4.
+        // Take advantage of the new method URLEncoder.encode(String, enc) if J2SE 1.4 is used.
+        try
+        {
+            Class urlEncoderClass = Class.forName("java.net.URLEncoder");
+            encodeMethod14 = urlEncoderClass.getMethod("encode", new Class[]{String.class, String.class});
+        }
+        catch (Throwable ex)
+        {
+            // encodeMethod14 will be null if exception
+        }
+    }
 
     /**
      * logger.
@@ -115,9 +136,7 @@ public class DefaultRequestHelper implements RequestHelper
 
             for (int i = 0; i < values.length; i++)
             {
-                // values[i] = URLEncoder.encode(values[i], "UTF-8");
-                // deprecated in java 1.4, but still need this for jre 1.3 compatibility
-                values[i] = URLEncoder.encode(StringUtils.defaultString(originalValues[i]));
+                values[i] = encodeURL(StringUtils.defaultString(originalValues[i]));
             }
             map.put(paramName, values);
 
@@ -127,5 +146,33 @@ public class DefaultRequestHelper implements RequestHelper
         return map;
     }
 
+    /**
+     * Called encodeUrl using j2se 1.4 version by reflection if available, or backward compatible version.
+     * @param url url to be encoded
+     * @return encoded url.
+     */
+    private String encodeURL(String url)
+    {
+        if (encodeMethod14 != null)
+        {
+            Object[] methodArgs = new Object[2];
+            methodArgs[0] = url;
+            methodArgs[1] = response.getCharacterEncoding();
+
+            try
+            {
+                return (String) encodeMethod14.invoke(null, methodArgs);
+            }
+            catch (Exception ex)
+            {
+                throw new RuntimeException("System error invoking URLEncoder.encode() by reflection.", ex);
+            }
+        }
+        else
+        {
+            // must use J2SE 1.3 version
+            return URLEncoder.encode(url);
+        }
+    }
 
 }
