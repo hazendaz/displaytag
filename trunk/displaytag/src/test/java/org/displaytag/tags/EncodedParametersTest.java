ecfc85d790930354460c6dc72c87ff204281f472
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2004-09-17 15:20:11 +0000

    testcase for 1016089 - Param values not URLEncoded

---
+++ trunk/displaytag/src/test/java/org/displaytag/tags/EncodedParametersTest.java
@@ -0,0 +1,61 @@
+package org.displaytag.tags;
+
+import org.displaytag.test.DisplaytagCase;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebLink;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import com.meterware.httpunit.WebTable;
+
+
+/**
+ * Tests for 1016089 - Param values not URLEncoded.
+ * @author Fabrizio Giustina
+ * @version $Revision$ ($Author$)
+ */
+public class EncodedParametersTest extends DisplaytagCase
+{
+
+    /**
+     * Instantiates a new test case.
+     * @param name test name
+     */
+    public EncodedParametersTest(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * @see org.displaytag.test.DisplaytagCase#getJspName()
+     */
+    public String getJspName()
+    {
+        return "encodedparameter.jsp";
+    }
+
+    /**
+     * Test link generated using column attributes.
+     * @param jspName jsp name, with full path
+     * @throws Exception any axception thrown during test.
+     */
+    public void doTest(String jspName) throws Exception
+    {
+        WebRequest request = new GetMethodWebRequest(jspName);
+
+        WebResponse response = runner.getResponse(request);
+        if (log.isInfoEnabled())
+        {
+            log.info(response.getText());
+        }
+
+        WebTable[] tables = response.getTables();
+        assertEquals("Expected one table in result.", 1, tables.length);
+
+        WebLink[] links = response.getLinks();
+        assertEquals("Wrong number of links in result.", 1, links.length);
+
+        assertEquals("Parameter in link should be encoded.", "/context/dynlink?param=1%2B1", links[0].getURLString());
+    }
+
+}
