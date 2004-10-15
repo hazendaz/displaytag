d314aa76f674a7f8fc6d27c932d97fa00c402773
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2004-10-15 22:15:17 +0000

    fixes DISPL-6 - Error: mediaType is already defined. To avoid errors when nested tables are used, the current mediaType is no more declared as a compile-time variable

--- displaytag/src/main/java/org/displaytag/tags/TableTagExtraInfo.java
+++ displaytag/src/main/java/org/displaytag/tags/TableTagExtraInfo.java
@@ -1,14 +1,12 @@
 package org.displaytag.tags;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.jsp.tagext.TagAttributeInfo;
 import javax.servlet.jsp.tagext.TagData;
 import javax.servlet.jsp.tagext.TagExtraInfo;
 import javax.servlet.jsp.tagext.VariableInfo;
-
-import org.displaytag.properties.MediaTypeEnum;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -16,7 +14,6 @@ import java.util.List;
  * <ul>
  * <li>table id = object contained in row</li>
  * <li>table id + ROWNUM_SUFFIX = row number</li>
- * <li>mediaType = current media</li>
  * </ul>
  * @author Fabrizio Giustina
  * @version $Revision$ ($Author$)
@@ -38,26 +35,21 @@ public class TableTagExtraInfo extends TagExtraInfo
      */
     public VariableInfo[] getVariableInfo(TagData data)
     {
-        List variables = new ArrayList(3);
+        List variables = new ArrayList(2);
 
-        Object tagId = data.getAttributeString(TagAttributeInfo.ID);
+        Object idObj = data.getAttribute(TagAttributeInfo.ID);
 
-        // if id is null don't define anything
-        if (tagId != null)
+        // avoid errors, but "id" and "id_rownum" will not be defined
+        if (idObj != TagData.REQUEST_TIME_VALUE && idObj != null)
         {
+            String tagId = data.getAttributeString(TagAttributeInfo.ID);
+
             // current row
             variables.add(new VariableInfo(tagId.toString(), Object.class.getName(), true, VariableInfo.NESTED));
 
             // current row number
             variables.add(new VariableInfo(tagId + ROWNUM_SUFFIX, Integer.class.getName(), true, VariableInfo.NESTED));
         }
-
-        // media type
-        variables.add(new VariableInfo(
-            TableTag.PAGE_ATTRIBUTE_MEDIA,
-            MediaTypeEnum.class.getName(),
-            true,
-            VariableInfo.NESTED));
 
         return (VariableInfo[]) variables.toArray(new VariableInfo[]{});
     }
