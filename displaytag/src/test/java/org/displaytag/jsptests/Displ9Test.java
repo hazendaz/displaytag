ca28db3b29df75af9cb57c0fc4a513ef82d4dd96
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2004-10-23 16:33:36 +0000

    added navigation test for DISPL-9

---
+++ displaytag/src/test/java/org/displaytag/jsptests/Displ9Test.java
@@ -0,0 +1,73 @@
+package org.displaytag.jsptests;
+
+import org.displaytag.tags.TableTagParameters;
+import org.displaytag.test.DisplaytagCase;
+import org.displaytag.util.ParamEncoder;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebLink;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+
+
+/**
+ * Tests for DISPL-9 - Send user back to Page 1 on Desc/Asc.
+ * @author Fabrizio Giustina
+ * @version $Revision$ ($Author$)
+ */
+public class Displ9Test extends DisplaytagCase
+{
+
+    /**
+     * Instantiates a new test case.
+     * @param name test name
+     */
+    public Displ9Test(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * @see org.displaytag.test.DisplaytagCase#getJspName()
+     */
+    public String getJspName()
+    {
+        return "DISPL-9.jsp";
+    }
+
+    /**
+     * Verifies that the generated page contains a table with the expected number of columns.
+     * @param jspName jsp name, with full path
+     * @throws Exception any axception thrown during test.
+     */
+    public void doTest(String jspName) throws Exception
+    {
+        WebRequest request = new GetMethodWebRequest(jspName);
+        ParamEncoder encoder = new ParamEncoder("table");
+        WebResponse response;
+
+        // step 0
+        response = runner.getResponse(request);
+
+        // link[0] is a column header
+        // link[1] is paging link
+
+        // 1. User clicks column 3 header to sort ascending. User returned to page 1.
+        response = response.getLinks()[0].click();
+        assertNotNull("Not in page one as expected", response.getElementWithID("PAGEONE"));
+
+        // 2. User clicks column 3 header to sort descending. User returned to page 1.
+        response = response.getLinks()[0].click();
+        assertNotNull("Not in page one as expected", response.getElementWithID("PAGEONE"));
+
+        // 3. User navigates to page other than page 1.
+        response = response.getLinks()[1].click();
+        assertNotNull("Not in page two as expected", response.getElementWithID("OTHERPAGE"));
+
+        // 4. User clicks column 3 header to sort ascending. User NOT returned to page 1, rather, user stays on current
+        // page number.
+        response = response.getLinks()[0].click();
+        assertNotNull("Not in page one as expected", response.getElementWithID("PAGEONE"));
+    }
+
+}
