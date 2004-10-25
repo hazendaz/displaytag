275267c42a12778f4d36eb8022f595cbb401cec9
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2004-10-25 19:58:21 +0000

    fixes DISPL-4, work started on DISPL-1

--- trunk/displaytag/src/test/java/org/displaytag/util/LinkUtilTest.java
+++ trunk/displaytag/src/test/java/org/displaytag/util/LinkUtilTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+
 /**
  * Test case for org.displaytag.util.LinkUtil.
  * @author Fabrizio Giustina
@@ -32,17 +33,13 @@ public class LinkUtilTest extends TestCase
      */
     public void testLongTextWithLink()
     {
-        String linked =
-            LinkUtil.autoLink(
-                "A large string of text. Foo bar. Foo bar. Foo bar. Foo bar. "
-                    + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. "
-                    + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. http://foo.bar.");
+        String linked = LinkUtil.autoLink("A large string of text. Foo bar. Foo bar. Foo bar. Foo bar. "
+            + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. "
+            + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. http://foo.bar.");
 
-        assertEquals(
-            "A large string of text. Foo bar. Foo bar. Foo bar. Foo bar. "
-                + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. "
-                + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. <a href=\"http://foo.bar.\">http://foo.bar.</a>",
-            linked);
+        assertEquals("A large string of text. Foo bar. Foo bar. Foo bar. Foo bar. "
+            + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. "
+            + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. <a href=\"http://foo.bar.\">http://foo.bar.</a>", linked);
     }
 
     /**
@@ -50,17 +47,13 @@ public class LinkUtilTest extends TestCase
      */
     public void testLongTextWithEmail()
     {
-        String linked =
-            LinkUtil.autoLink(
-                "A large string of text. Foo bar. Foo bar. Foo bar. Foo bar. "
-                    + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. "
-                    + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. foo@bar.com.");
+        String linked = LinkUtil.autoLink("A large string of text. Foo bar. Foo bar. Foo bar. Foo bar. "
+            + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. "
+            + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. foo@bar.com.");
 
-        assertEquals(
-            "A large string of text. Foo bar. Foo bar. Foo bar. Foo bar. "
-                + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. "
-                + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. <a href=\"mailto:foo@bar.com.\">foo@bar.com.</a>",
-            linked);
+        assertEquals("A large string of text. Foo bar. Foo bar. Foo bar. Foo bar. "
+            + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. "
+            + "Foo bar. Foo bar. Foo bar. Foo bar. Foo bar. <a href=\"mailto:foo@bar.com.\">foo@bar.com.</a>", linked);
     }
 
     /**
@@ -111,6 +104,136 @@ public class LinkUtilTest extends TestCase
         log.debug(linked);
 
         assertEquals("<a href=\"http://foo.bar\">http://foo.bar</a> .", linked);
+    }
+
+    /**
+     * Test for the stripHTMLTags() method.
+     */
+    public void testStripHtmlTagsSimple()
+    {
+        assertEquals("well done", LinkUtil.stripHTMLTags("<strong>well</strong> done"));
+    }
+
+    /**
+     * Test for the stripHTMLTags() method.
+     */
+    public void testStripHtmlTagsNoHtml()
+    {
+        assertEquals("well done", LinkUtil.stripHTMLTags("well done"));
+    }
+
+    /**
+     * Test for the stripHTMLTags() method.
+     */
+    public void testStripHtmlTagsMultiple()
+    {
+        assertEquals("well done again", LinkUtil
+            .stripHTMLTags("<a href=\"go\"><strong>well <em>done</em></strong> again</a>"));
+    }
+
+    /**
+     * Test for the stripHTMLTags() method.
+     */
+    public void testStripHtmlUnbalancedOpen()
+    {
+        assertEquals("<well done", LinkUtil.stripHTMLTags("<well done"));
+    }
+
+    /**
+     * Test for the stripHTMLTags() method.
+     */
+    public void testStripHtmlUnbalancedClosed()
+    {
+        assertEquals("well> done", LinkUtil.stripHTMLTags("well> done"));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringByLength() method.
+     */
+    public void testAbbreviateHtmlStringByLengthNoHtml()
+    {
+        assertEquals("well...", LinkUtil.abbreviateHtmlString("well done", 4, false));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringByLength() method.
+     */
+    public void testAbbreviateHtmlStringByLengthShorter()
+    {
+        assertEquals("well done", LinkUtil.abbreviateHtmlString("well done", 9, false));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringByLength() method.
+     */
+    public void testAbbreviateHtmlStringByLengthHtmlShorter()
+    {
+        assertEquals("<strong>well</strong> done", LinkUtil.abbreviateHtmlString(
+            "<strong>well</strong> done",
+            10,
+            false));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringHtmlSimple() method.
+     */
+    public void testAbbreviateHtmlStringByLengthHtmlSimple()
+    {
+        assertEquals("<strong>well...</strong>", LinkUtil.abbreviateHtmlString("<strong>well done</strong>", 4, false));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringHtmlSimple() method.
+     */
+    public void testAbbreviateHtmlStringByLengthHtmlComplex()
+    {
+        assertEquals("<a href=\\\"link\\\">well...</a>", LinkUtil.abbreviateHtmlString(
+            "<a href=\"link\">well <strong>done<strong></a>",
+            4,
+            false));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringByNumberOfWords() method.
+     */
+    public void testAbbreviateHtmlStringByNumberOfWordsNoHtml()
+    {
+        assertEquals("well...", LinkUtil.abbreviateHtmlString("well done", 1, true));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringByNumberOfWords() method.
+     */
+    public void testAbbreviateHtmlStringByNumberOfWordsShorter()
+    {
+        assertEquals("well done", LinkUtil.abbreviateHtmlString("well done", 2, true));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringByNumberOfWords() method.
+     */
+    public void testAbbreviateHtmlStringByNumberOfWordsHtmlShorter()
+    {
+        assertEquals("<strong>well</strong> done", LinkUtil.abbreviateHtmlString("<strong>well</strong> done", 3, true));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringByNumberOfWords() method.
+     */
+    public void testAbbreviateHtmlStringByLengthNumberOfWordsHtmlSimple()
+    {
+        assertEquals("<strong>well...</strong>", LinkUtil.abbreviateHtmlString("<strong>well done</strong>", 1, true));
+    }
+
+    /**
+     * Test for the abbreviateHtmlStringByNumberOfWords() method.
+     */
+    public void testAbbreviateHtmlStringByNumberOfWordsHtmlComplex()
+    {
+        assertEquals("<a href=\\\"link\\\">well...</a>", LinkUtil.abbreviateHtmlString(
+            "<a href=\"link\">well <strong>done<strong></a>",
+            1,
+            true));
     }
 
 }
