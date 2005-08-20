328e479a0e0ff611eea54ee6ce254fb3c07b8c2c
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2005-08-20 10:16:43 +0000

    working to the m2 build, update pom, uploaded a custom timestamped build of httpunit. Trying to make servletunit tests work with tomcat 5.5 and the integrated JDT compiler

--- trunk/displaytag/src/test/java/org/displaytag/jsptests/PartialListTest.java
+++ trunk/displaytag/src/test/java/org/displaytag/jsptests/PartialListTest.java
@@ -1,14 +1,14 @@
 package org.displaytag.jsptests;
 
+import org.displaytag.tags.TableTagParameters;
+import org.displaytag.test.DisplaytagCase;
+import org.displaytag.util.ParamEncoder;
+
 import com.meterware.httpunit.GetMethodWebRequest;
 import com.meterware.httpunit.WebLink;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
 import com.meterware.httpunit.WebTable;
-import org.displaytag.test.DisplaytagCase;
-import org.displaytag.util.ParamEncoder;
-import org.displaytag.tags.TableTagParameters;
-import junit.framework.TestCase;
 
 
 /**
@@ -37,7 +37,7 @@ public class PartialListTest extends DisplaytagCase
     {
 
         WebRequest request = new GetMethodWebRequest(jspName);
-        ParamEncoder p1 = new ParamEncoder("table");
+
         ParamEncoder p2 = new ParamEncoder("table2");
         ParamEncoder p3 = new ParamEncoder("table3");
 
@@ -55,25 +55,25 @@ public class PartialListTest extends DisplaytagCase
         WebLink[] links = response.getLinks();
         WebTable[] tables = response.getTables();
         // ensure all our search bar links contain page 2 as the param since we only have 2 pages
-        TestCase.assertEquals("2", links[0].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
-        TestCase.assertEquals("2", links[1].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
-        TestCase.assertEquals("2", links[2].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
-        TestCase.assertEquals(3, tables[0].getRowCount()); // title row + 2 data's
-        TestCase.assertEquals("1", tables[0].getCellAsText(1, 0));
-        TestCase.assertEquals("4", tables[0].getCellAsText(2, 0));
+        assertEquals("2", links[0].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
+        assertEquals("2", links[1].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
+        assertEquals("2", links[2].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
+        assertEquals(3, tables[0].getRowCount()); // title row + 2 data's
+        assertEquals("1", tables[0].getCellAsText(1, 0));
+        assertEquals("4", tables[0].getCellAsText(2, 0));
 
         // second table assertions
         // links should point to first page
-        TestCase.assertEquals("1", links[4].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
-        TestCase.assertEquals("1", links[5].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
-        TestCase.assertEquals("1", links[6].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
-        TestCase.assertEquals(3, tables[1].getRowCount()); // title row + 2 data's
-        TestCase.assertEquals("1", tables[1].getCellAsText(1, 0));
-        TestCase.assertEquals("4", tables[1].getCellAsText(2, 0));
+        assertEquals("1", links[4].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
+        assertEquals("1", links[5].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
+        assertEquals("1", links[6].getParameterValues(p2.encodeParameterName(TableTagParameters.PARAMETER_PAGE))[0]);
+        assertEquals(3, tables[1].getRowCount()); // title row + 2 data's
+        assertEquals("1", tables[1].getCellAsText(1, 0));
+        assertEquals("4", tables[1].getCellAsText(2, 0));
 
         // third table assertions
-        TestCase.assertEquals(3, tables[2].getRowCount()); // title row + 2 data's
-        TestCase.assertEquals("4", tables[2].getCellAsText(1, 0));
-        TestCase.assertEquals("1", tables[2].getCellAsText(2, 0));
+        assertEquals(3, tables[2].getRowCount()); // title row + 2 data's
+        assertEquals("4", tables[2].getCellAsText(1, 0));
+        assertEquals("1", tables[2].getCellAsText(2, 0));
     }
 }
