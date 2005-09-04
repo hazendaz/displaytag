6ebe8d6c3302cb8cf6ff08eb99e6a24fd373adf3
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2005-09-04 09:35:45 +0000

    implemented tests for DISPL-224

--- trunk/displaytag/src/test/java/org/displaytag/jsptests/Displ224Test.java
+++ trunk/displaytag/src/test/java/org/displaytag/jsptests/Displ224Test.java
@@ -1,7 +1,12 @@
 package org.displaytag.jsptests;
 
-import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang.StringUtils;
 import org.displaytag.test.DisplaytagCase;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import com.meterware.httpunit.WebTable;
 
 
 /**
@@ -27,14 +32,18 @@ public class Displ224Test extends DisplaytagCase
      */
     public void doTest(String jspName) throws Exception
     {
-        // a reminder, test is missing
-        throw new NotImplementedException("test not implemented");
 
-        // WebRequest request = new GetMethodWebRequest(jspName);
-        // WebResponse response = runner.getResponse(request);
-        //
-        // WebTable[] tables = response.getTables();
-        // assertEquals("Expected 1 table in result.", 1, tables.length);
+        WebRequest request = new GetMethodWebRequest(jspName);
+        WebResponse response = runner.getResponse(request);
+
+        WebTable[] tables = response.getTables();
+        assertEquals("Wrong number of tables in result.", 1, tables.length);
+
+        assertEquals("Wrong number of rows in result.", 2, tables[0].getRowCount());
+        assertEquals("col", tables[0].getTableCell(0, 0).getAttribute("scope"));
+        assertEquals(StringUtils.EMPTY, tables[0].getTableCell(0, 1).getAttribute("scope"));
+        assertEquals(StringUtils.EMPTY, tables[0].getTableCell(1, 0).getAttribute("scope"));
+        assertEquals("row", tables[0].getTableCell(1, 1).getAttribute("scope"));
 
     }
 
