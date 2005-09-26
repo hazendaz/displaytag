ab18758b1c1649941d49797c3a2bbc5f95dcf24d
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2005-09-26 19:53:35 +0000

    remove useless "public" declaration

--- trunk/displaytag/src/main/java/org/displaytag/pagination/PaginatedList.java
+++ trunk/displaytag/src/main/java/org/displaytag/pagination/PaginatedList.java
@@ -27,38 +27,38 @@ public interface PaginatedList
      * Returns the current partial list
      * @return the current partial list
      */
-    public List getList();
+    List getList();
 
     /**
      * Returns the page number of the partial list (starts from 1)
      * @return the page number of the partial list
      */
-    public int getPageNumber();
+    int getPageNumber();
 
     /**
      * Returns the number of objects per page. Unless this page is the last one the partial list should thus have a size
      * equal to the result of this method
      * @return the number of objects per page
      */
-    public int getObjectsPerPage();
+    int getObjectsPerPage();
 
     /**
      * Returns the size of the full list
      * @return the size of the full list
      */
-    public int getFullListSize();
+    int getFullListSize();
 
     /**
      * Returns the sort criterion used to externally sort the full list
      * @return the sort criterion used to externally sort the full list
      */
-    public String getSortCriterion();
+    String getSortCriterion();
 
     /**
      * Returns the sort direction used to externally sort the full list
      * @return the sort direction used to externally sort the full list
      */
-    public SortOrderEnum getSortDirection();
+    SortOrderEnum getSortDirection();
 
     /**
      * Returns an ID for the search used to get the list. It may be null. Such an ID can be necessary if the full list
@@ -66,5 +66,5 @@ public interface PaginatedList
      * the full list from the cache
      * @return the search ID
      */
-    public String getSearchId();
+    String getSearchId();
 }
