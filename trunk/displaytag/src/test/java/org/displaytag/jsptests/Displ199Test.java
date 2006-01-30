d643c87aa4292932cb2253c7e2130e99d2475f63
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2006-01-30 23:10:11 +0000

    more tests with totals

--- trunk/displaytag/src/test/java/org/displaytag/jsptests/Displ199Test.java
+++ trunk/displaytag/src/test/java/org/displaytag/jsptests/Displ199Test.java
@@ -3,6 +3,7 @@ package org.displaytag.jsptests;
 import org.displaytag.test.DisplaytagCase;
 
 import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.HTMLElement;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
 import com.meterware.httpunit.WebTable;
@@ -42,6 +43,9 @@ public class Displ199Test extends DisplaytagCase
 
         WebTable[] tables = response.getTables();
         assertEquals("Wrong number of tables.", 1, tables.length);
+
+        HTMLElement divtotal = response.getElementWithID("divtotal");
+        assertEquals("7", divtotal.getText());
 
         // @todo
         // test using decorator, currenlty in the sample webapp
