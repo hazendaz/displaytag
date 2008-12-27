2f9a56bb5ee78a0e77bb906004f44b108cfb56c3
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2008-12-27 14:56:17 +0000

    major cleanup after 1.2

--- trunk/displaytag/src/test/java/org/displaytag/jsptests/FilterTest.java
+++ trunk/displaytag/src/test/java/org/displaytag/jsptests/FilterTest.java
@@ -5,7 +5,6 @@ import org.apache.log4j.Logger;
 import org.displaytag.properties.MediaTypeEnum;
 import org.displaytag.tags.TableTag;
 import org.displaytag.tags.TableTagParameters;
-import org.displaytag.tags.el.ELTableTag;
 import org.displaytag.test.DisplaytagCase;
 import org.displaytag.util.ParamEncoder;
 
@@ -44,13 +43,11 @@ public class FilterTest extends DisplaytagCase
 
         // save previous level, since we are expeting an excetion I don't want to fill logs
         Level previousLevel = Logger.getLogger(TableTag.class).getLevel();
-        Level previousLevelEL = Logger.getLogger(ELTableTag.class).getLevel();
 
         try
         {
             // disable log
             Logger.getLogger(TableTag.class).setLevel(Level.OFF);
-            Logger.getLogger(ELTableTag.class).setLevel(Level.OFF);
 
             // check if page need a filter (unfiltered request)
             runner.getResponse(request);
@@ -63,7 +60,6 @@ public class FilterTest extends DisplaytagCase
 
         // reset log
         Logger.getLogger(TableTag.class).setLevel(previousLevel);
-        Logger.getLogger(ELTableTag.class).setLevel(previousLevelEL);
 
         request = new GetMethodWebRequest(jspName);
         request.setParameter(mediaParameter, Integer.toString(MediaTypeEnum.XML.getCode()));
