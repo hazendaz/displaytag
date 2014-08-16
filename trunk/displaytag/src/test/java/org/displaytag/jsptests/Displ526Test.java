a2c035ea3acbc905a4afef3bd75c9c6e64adba59
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2014-08-16 21:04:35 +0000

    testcase for DISPL-526

---
+++ trunk/displaytag/src/test/java/org/displaytag/jsptests/Displ526Test.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2002-2014 Fabrizio Giustina, the Displaytag team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.displaytag.jsptests;
+
+import org.displaytag.tags.TableTagParameters;
+import org.displaytag.test.DisplaytagCase;
+import org.displaytag.util.ParamEncoder;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import com.meterware.httpunit.WebTable;
+
+
+/**
+ * Tests for DISPL-526 - cell value that is the same as the one vertically above is omitted.
+ * @author Fabrizio Giustina
+ * @version $Revision: 1081 $ ($Author: fgiust $)
+ */
+public class Displ526Test extends DisplaytagCase
+{
+
+    /**
+     * @see org.displaytag.test.DisplaytagCase#getJspName()
+     */
+    public String getJspName()
+    {
+        return "DISPL-526.jsp";
+    }
+
+    /**
+     * Check addictional parameters in paging.banner.*.
+     * @param jspName jsp name, with full path
+     * @throws Exception any axception thrown during test.
+     */
+    @Override
+    @Test
+    public void doTest() throws Exception
+    {
+        WebRequest request = new GetMethodWebRequest(getJspUrl(getJspName()));
+
+        WebResponse response;
+
+        response = runner.getResponse(request);
+
+        if (log.isDebugEnabled())
+        {
+            log.debug(response.getText());
+        }
+
+        WebTable[] tables = response.getTables();
+        Assert.assertEquals(1, tables.length);
+
+        Assert.assertEquals("Wrong column content", "1", tables[0].getCellAsText(2, 1));
+
+    }
+
+}
