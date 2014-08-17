f07b7140c6ad3ecf6926358debd35a402f1bc429
fgiust <fgiust@cff2dd9d-f205-4dd1-ae1c-e65f0af7b6ac>
2014-08-17 09:50:42 +0000

    DISPL-505 Class cache usage causes make using different factory for different tables not work

---
+++ trunk/displaytag/src/test/java/org/displaytag/jsptests/Displ505Test.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) 2002-2014 Fabrizio Giustina, the Displaytag team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.displaytag.jsptests;
+
+import org.displaytag.decorator.DateColumnDecorator;
+import org.displaytag.test.DisplaytagCase;
+import org.displaytag.test.KnownTypes;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import com.meterware.httpunit.WebTable;
+
+
+/**
+ * Tests for DISPL-505 - Class cache usage causes make using different factory for different tables not work.
+ * @author Mike Calmus
+ * @version $Revision: 1081 $ ($Author: fgiust $)
+ */
+public class Displ505Test extends DisplaytagCase
+{
+
+    /**
+     * @see org.displaytag.test.DisplaytagCase#getJspName()
+     */
+    public String getJspName()
+    {
+        return "DISPL-505.jsp";
+    }
+
+    /**
+     * Check additional parameters in urls.
+     * @param jspName jsp name, with full path
+     * @throws Exception any Exception thrown during test.
+     */
+    @Override
+    @Test
+    public void doTest() throws Exception
+    {
+        WebRequest request = new GetMethodWebRequest(getJspUrl(getJspName()));
+
+        WebResponse response = runner.getResponse(request);
+
+        if (log.isDebugEnabled())
+            log.debug("RESPONSE: " + response.getText());
+
+        WebTable[] tables = response.getTables();
+
+        Assert.assertEquals("Wrong number of tables.", 2, tables.length);
+
+        Assert.assertEquals(
+            "Expected decorated value not found.",
+            new DateColumnDecorator().decorate(KnownTypes.TIME_VALUE, null, null),
+            tables[0].getCellAsText(1, 0));
+
+        Assert.assertEquals(
+            "Expected decorated value not found.",
+            KnownTypes.TIME_VALUE.toString(),
+            tables[1].getCellAsText(1, 0));
+    }
+}
--- trunk/src/site/markdown/dependencies.md
+++ trunk/src/site/markdown/dependencies.md
@@ -1,5 +1,4 @@
-Dependencies
-------------
+# Dependencies
 
 See [displaytag/dependencies.html](displaytagdependencies.html) for the
 list of dependencies needed by displaytag.
@@ -9,19 +8,39 @@ following to the dependencies section in your POM. Maven will take care
 of including all the needed libraries for you.
 
 ```xml
-        <dependency>
-          <groupId>org.displaytag</groupId>
-          <artifactId>displaytag</artifactId>
-          <version>2.0</version>
-        </dependency>
+<dependency>
+  <groupId>org.displaytag</groupId>
+  <artifactId>displaytag</artifactId>
+  <version>2.0</version>
+</dependency>
 ```
 
-### Optional modules
+Since displaytag uses [http://www.slf4j.org/](slf4j) for logging you will
+also need to choose a slf4j logging implementation (by default displaytag dependencies
+will only include slf4j-api, in order to let you choose your preferred implementation).
+You usually may want to include the log4j adapter, using:
 
-Starting from the 1.1 release optional modules are distributed as
-additional jars. Any module could require additional dependencies:
+```xml
+<dependency>
+  <groupId>org.slf4j</groupId>
+  <artifactId>slf4j-log4j12</artifactId>
+  <version>1.7.7</version>
+</dependency>
+```
 
--   [displaytag portlet](displaytag-portletdependencies.html)
--   [displaytag excel export
-    module](#displaytag-export-poidependencies.html)
+## Optional dependencies
 
+By default Displaytag render excel files using a text-based format. You may need to enable a different exporter
+using excel binary format: the exporter is included by default in the distribution but you will also need to include
+the `poi` dependency in order to use it:
+
+```xml
+<dependency>
+  <groupId>org.apache.poi</groupId>
+  <artifactId>poi</artifactId>
+  <version>3.10-FINAL</version>
+</dependency>
+```
+
+(the export-poi module was distributed in a separate jar before version 2.0. Since displaytag 2.0 the module is not
+required since it has been included in the main jar)
