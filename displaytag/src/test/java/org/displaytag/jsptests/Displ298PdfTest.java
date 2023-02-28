f48df138c848e194ca646e30828d87f88482f0c6
Jeremy Landis <jeremylandis@hotmail.com>
2023-02-27 21:35:19 -0500

    WIP - itext 5 to 7 migration

--- displaytag/src/test/java/org/displaytag/jsptests/Displ298PdfTest.java
+++ displaytag/src/test/java/org/displaytag/jsptests/Displ298PdfTest.java
@@ -21,11 +21,14 @@
  */
 package org.displaytag.jsptests;
 
-import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
 import com.meterware.httpunit.GetMethodWebRequest;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
 import org.displaytag.export.ExportViewFactory;
@@ -81,11 +84,10 @@ class Displ298PdfTest extends DisplaytagCase {
         Assertions.assertEquals("application/pdf", response.getContentType(), "Expected a different content type.");
 
         final InputStream stream = response.getInputStream();
-        final byte[] result = new byte[3000];
-        stream.read(result);
+        final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 
-        final PdfReader reader = new PdfReader(result);
-        Assertions.assertEquals(1, reader.getNumberOfPages(), "Expected a valid pdf file with a single page");
+        final PdfDocument pdfDoc = new PdfDocument(new PdfReader(stream), new PdfWriter(outStream));
+        Assertions.assertEquals(1, pdfDoc.getNumberOfPages(), "Expected a valid pdf file with a single page");
     }
 
 }
